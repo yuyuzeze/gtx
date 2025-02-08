dc26071ef83cb3b3a17a3df68a726aef5eb6bb84
yuyuzeze <yuyuzeze@gmail.com>
2025-02-08 22:37:41 +0900

    oliospec

---
+++ oliospec.py
@@ -0,0 +1,57 @@
+import requests
+import os
+import random
+from notify import send
+from bs4 import BeautifulSoup
+
+def get_proxy():
+    urls_str = os.getenv('sock_proxy', '')
+    
+    # 使用换行符分割 URL
+    urls = [url.strip() for url in urls_str.split('\n') if url.strip()]
+    #随机选择一个ip
+    url = random.choice(urls)
+    #获取ip地址并打印
+    response = requests.get("https://api.ipify.org", proxies={"http": url, "https": url} )
+    print("代理ip地址：",response.text)
+    return url
+
+def parse_gpu_data(bs):
+    result = []
+    
+    ul = bs.find('ul', class_='innerList')
+    for item in ul.select('li > .innerBox'):
+        if not item.find('p', class_='quantity').text.strip():
+            continue
+        else:
+            name_ele = item.find('p', class_='name')
+            item_data = {
+                "name": name_ele.text.strip(),
+                "url": 'https://www.oliospec.com' + name_ele.find('a')['href']
+            }
+            result.append(item_data)
+
+    return result
+
+def get_applied_stock(url):
+    proxy = get_proxy()
+    response = requests.get(url, proxies={"http": proxy, "https": proxy})   
+    bs = BeautifulSoup(response.text, 'html.parser')
+    gpu_data = parse_gpu_data(bs)
+    return gpu_data;
+
+if __name__ == "__main__":
+    url =  os.getenv('oliospec_url', '')
+    stock_info = get_applied_stock(url)
+
+    for item in stock_info:
+        print(item['name'])
+        send(
+                title='🎯 olio.spec 发现目标商品！',
+                content=f'''## {item['name']}
+
+> **直达链接：**
+> [点击购买]({item['url']})
+
+---'''
+            )
